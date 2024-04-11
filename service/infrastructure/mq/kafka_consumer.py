df3a2cc3170d39f6cc1623efc20d65df2ac1d5ea
netdong-li <east2050@gmail.com>
2024-04-12 01:14:12 +0800

    Fix topic creation issue with adminclient

--- service/infrastructure/mq/kafka_consumer.py
+++ service/infrastructure/mq/kafka_consumer.py
@@ -1,7 +1,9 @@
 import sys
+import time
 from typing import List
 
-from confluent_kafka import Consumer, KafkaException
+from confluent_kafka import Consumer, KafkaException, KafkaError
+from confluent_kafka.admin import AdminClient, NewTopic
 
 from ...domain.gateway import TrendEventConsumer, ConsumeCallback
 
@@ -12,10 +14,20 @@ class KafkaConsumer(TrendEventConsumer):
             {'bootstrap.servers': ','.join(brokers),
              'group.id': group_id,
              'auto.offset.reset': 'smallest'})
+        self.brokers = brokers
         self.topic = topic
         self.running = False
 
+    def _try_to_create_topic(self, brokers: List[str], topic: str):
+        admin_client = AdminClient({'bootstrap.servers': ','.join(brokers)})
+        topic_metadata = admin_client.list_topics(timeout=10)
+        if topic_metadata.topics.get(topic) is None:
+            new_topic = NewTopic(topic, 1, 1)
+            admin_client.create_topics([new_topic])
+            time.sleep(1)  # Hack: wait for it
+
     def consume_events(self, callback: ConsumeCallback):
+        self._try_to_create_topic(self.brokers, self.topic)
         try:
             self.consumer.subscribe([self.topic])
             self.running = True
@@ -24,7 +36,7 @@ class KafkaConsumer(TrendEventConsumer):
                 if msg is None:
                     continue
                 if msg.error():
-                    if msg.error().code() == KafkaException._PARTITION_EOF:
+                    if msg.error().code() == KafkaError._PARTITION_EOF:
                         # End of partition
                         sys.stderr.write('%% {} [{}] reached end at offset {} - {}\n'.format(
                             msg.topic(), msg.partition(), msg.offset()))
